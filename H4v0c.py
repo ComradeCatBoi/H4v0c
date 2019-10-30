3f05996eda1b867e2f5a08c274655d1d362e1300
H4l0g3n <56747176+H4l0g3n@users.noreply.github.com>
2019-10-29 21:30:01 -0400

    Debug

--- H4v0c.py
+++ H4v0c.py
@@ -3,23 +3,19 @@
 import socket
 
 
-def main():
-    print(greeting())
-    lanScan()
-
-
 def greeting():
-    print(""" _   _   ___       _____      
+    print("""
+     _   _   ___       _____      
     | | | | /   |     |  _  |     
     | |_| |/ /| |_   _| |/' | ___ 
     |  _  / /_| \ \ / /  /| |/ __|
     | | | \___  |\ V /\ |_/ / (__ 
     \_| |_/   |_/ \_/  \___/ \___|
     """)
-    print("Wifi pentesting suite")
-    input("Press enter to continue")
+    print("     Wifi pentesting suite")
+    input("     Press enter to continue")
     print("\n"
-          "1. LAN Scan - Enumerate devices on your local network\n"
+          "     1. LAN Scan - Enumerate devices on your local network\n"
           # "# 2. Port scan -  \n"
           "\n"
           "\n")
@@ -30,3 +26,6 @@ def lanScan():
     localAddress = socket.gethostbyname(socket.gethostname())
     print("Local IP: " + localAddress)
     input()
+
+greeting()
+lanScan()
